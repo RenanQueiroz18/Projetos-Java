5132b9f215c74173abce15cce9d760ddecd6671a
Renan Ribeiro <queirozrenan27@gmail.com>
2024-08-19 15:46:12 -0400

    Criadas as Classes

---
+++ Animal.java
@@ -0,0 +1,36 @@
+public class Animal {
+    private String nome;
+    private String tamanho;
+    private String cor;
+
+    public String getNome(){
+        return this.nome;
+    }
+    public String getTamnho(){
+        return this.tamanho;
+    }
+    public String getCor(){
+        return this.cor;
+    }
+
+    public void setNome(String nomeNovo){
+        this.nome = nomeNovo;
+    }
+    public void setTamanho(String tamanhoNovo){
+        this.tamanho = tamanhoNovo;
+    }
+    public void setCor(String corNovo){
+        this.cor = corNovo;
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
